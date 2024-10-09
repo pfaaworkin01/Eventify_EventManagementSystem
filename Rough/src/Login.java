5071603d9ab40a2e12e88154dee83e43bdab8072
Prince Fahim Al-Araf <princefahim@iut-dhaka.edu>
2024-10-09 22:39:41 +0600

    First commit. Basic Login system implemented.

---
+++ Rough/src/Login.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Login {
+
+    private static final String LOGIN_CREDENTIALS = "Login_Credentials.txt";
+
+    public void register() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Username: ");
+        String username = scanner.nextLine();
+        while (!username.matches("[a-zA-Z0-9]+")) {
+            System.out.println("Invalid Username. Your username can only contain alphanumeric characters!!!");
+            username = scanner.nextLine();
+        }
+
+        System.out.println("Enter Password: ");
+        String password = scanner.nextLine();
+        while (!password.matches("[a-zA-Z0-9]+")) {
+            System.out.println("Invalid Password!!!");
+            password = scanner.nextLine();
+        }
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(LOGIN_CREDENTIALS, true))) {
+            writer.write(username + ":" + password + "\n");
+            System.out.println("Registered Successfully!!!");
+        } catch (IOException e) {
+            System.out.println("Error Registering User!!!");
+        }
+
+    }
+
+    public void login() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Username: ");
+        String username = scanner.nextLine();
+
+        System.out.println("Enter Password: ");
+        String password = scanner.nextLine();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(LOGIN_CREDENTIALS))) {
+            String line;
+            boolean found = false;
+
+            while((line = reader.readLine()) != null) {
+                String[] parts = line.split(":");
+                if(username.equals(parts[0]) && password.equals(parts[1])) {
+                    found = true;
+                    System.out.println("Logged In Successfully!!!");
+                    break;
+                }
+            }
+
+            if(!found) {
+                System.out.println("Invalid Username or Password!!!");
+            }
+            } catch (IOException e) {
+                System.out.println("Error Logging In!!!");
+            }
+
+    }
+
+}
---
+++ Rough/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        Login login = new Login();
+
+        while (!exit) {
+            System.out.println("\n");
+            System.out.println("\t\tSelect an Option:\n");
+            System.out.println("\t\t1. Register");
+            System.out.println("\t\t2. Login");
+            System.out.println("\t\t3. Exit");
+            System.out.println("\n");
+
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case "1":
+                    login.register();
+                    break;
+                case "2":
+                    login.login();
+                    break;
+                case "3":
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        }
+
+        scanner.close();
+    }
+
+}
