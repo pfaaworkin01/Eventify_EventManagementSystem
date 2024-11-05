cfad7fb6c89f110d38693fa1cf2c459be43201f0
Adib Rahman <adibrahman44@gmail.com>
2024-11-06 03:49:36 +0600

    Removed calling manageTeams method from main and inserted into TeamManager class .

--- Rough/src/Team/CreativeSector.java
+++ Rough/src/Team/CreativeSector.java
@@ -1,7 +1,9 @@
 package Team;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class CreativeSector extends Sector {
+public class CreativeSector extends Sector implements Serializable {
+    private static final long serialVersionUID = 1L;
     public CreativeSector() {
         super("Creative", new ArrayList<>());
     }
--- Rough/src/Team/CustomSector.java
+++ Rough/src/Team/CustomSector.java
@@ -1,8 +1,10 @@
 package Team;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class CustomSector extends Sector  {
+public class CustomSector extends Sector implements Serializable {
+    private static final long serialVersionUID = 1L;
     public CustomSector(String sectorName) {
         super(sectorName, new ArrayList<>());
     }
