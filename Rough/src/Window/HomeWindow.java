1382e41ea31d6b84479d9b7112055546c69cd840
Prince Fahim Al-Araf <princefahim@iut-dhaka.edu>
2024-11-06 12:08:10 +0600

    optimized Event window

--- Rough/src/Window/HomeWindow.java
+++ Rough/src/Window/HomeWindow.java
@@ -6,19 +6,16 @@ public class HomeWindow implements Window {
     public void showWindow() {
         System.out.println("\n");
         System.out.println("*".repeat(147));
-        printCentered("EVENTIFY");
-        printCentered("Make Every Event Count");
+        Window.printCentered("EVENTIFY");
+        Window.printCentered("Make Every Event Count");
         System.out.println("*".repeat(147));
         System.out.println("\n");
-        printCentered("HOME\n");
+        Window.printCentered("HOME\n");
         System.out.println(" ".repeat(65) + "1. User Registration");
         System.out.println(" ".repeat(65) + "2. User Login");
         System.out.println(" ".repeat(65) + "3. Quit\n");
     }
 
-    public static void printCentered(String text) {
-        int terminalWidth = 150;
-        int padding = (terminalWidth - text.length()) / 2;
-        System.out.println(" ".repeat(Math.max(0, padding)) + text);
-    }
+    @Override
+    public void takeInput() {}
 }
