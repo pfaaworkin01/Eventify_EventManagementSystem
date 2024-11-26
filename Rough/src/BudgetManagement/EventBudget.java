5689b6d869aec08324a98447e70939220bfda562
Adib Rahman <adibrahman44@gmail.com>
2024-11-26 16:29:57 +0600

    Updated EventBudget
    wrote constructor for the class.

--- Rough/src/BudgetManagement/EventBudget.java
+++ Rough/src/BudgetManagement/EventBudget.java
@@ -1,4 +1,16 @@
 package BudgetManagement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventBudget {
+    private String eventName;
+    private double totalBudget;
+    private List<DepartmentBudget> departments;
+
+    public EventBudget(String eventName, double totalBudget) {
+        this.eventName = eventName;
+        this.totalBudget = totalBudget;
+        this.departments = new ArrayList<>();
+    }
 }
